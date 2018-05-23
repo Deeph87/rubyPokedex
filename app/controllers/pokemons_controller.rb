9818715d014d6aad944dd6d54bd905ff4c2323c0
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2018-05-23 14:58:27 +0200

    CRUD done + début d'associations

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,8 +1,49 @@
 class PokemonsController < ApplicationController
+  before_action :set_pokemon, only: %i[show edit update destroy]
+
   def index
     @pokemons = Pokemon.all
   end
-  def show
+
+  def show; end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    @pokemon = Pokemon.new(
+        pokemon_params
+    )
+    if @pokemon.save
+      redirect_to @pokemon
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @pokemon.update(pokemon_params)
+      redirect_to @pokemon
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pokemon.destroy
+    redirect_to pokemons_url
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :level, :number, :health_points)
+  end
+
+  def set_pokemon
     @pokemon = Pokemon.find(params[:id])
   end
 end
