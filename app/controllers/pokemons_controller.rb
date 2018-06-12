5bdfba9fd3adb7e02216ba7a08705c149ae537db
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2018-06-12 10:43:25 +0200

    imporvement with helper and optimizations requests

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,7 +2,7 @@ class PokemonsController < ApplicationController
   before_action :set_pokemon, only: %i[show edit update destroy]
 
   def index
-    @pokemons = Pokemon.all
+    @pokemons = Pokemon.includes(:type)
   end
 
   def show; end
